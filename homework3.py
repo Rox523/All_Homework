abcbe750133de7a557724fb42b8105d4772c90ad
Rox523 <www.feart39@gmail.com>
2024-08-04 14:37:35 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+name = 'Daniil'
+print(name)
+age = 26
+print(age)
+new_age =print(age + 1)
+is_student = True
+print(is_student)
---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+while True:
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    my_list = int(input("Введите число"))
+    if my_list > -1:
+        print("Это число подходит")
+    else:
+        print("Неверное число")
+        break
