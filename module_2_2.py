d6150c59c2c7086b79a0d70a6339c7119d0a0f28
Rox523 <www.feart39@gmail.com>
2024-08-11 12:42:50 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,14 +1,9 @@
-#first, ssecond,third
-first = 1
-second = 3
-third = 5
 a = int(input('Введите первое число'))
 b = int(input('Введите второе число '))
 c = int(input('Введите третье число '))
-if a % 3 == 0 and  a % 4 == 0 and  a % 6 == 0:
-    print(3)
-elif b % 3 == 0 or b % 4== 0 or b % 6 == 0:
-    print(2)
-elif c % 3 == 0 or b % 4== 0 or b % 6 == 0:
+if a == b == c:
+     print(3)
+elif a == b or c == a or b == c :
+     print(2)
+else:
     print(0)
-
