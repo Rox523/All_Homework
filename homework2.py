59894fe853a25680ed12aa77ac2311283ca8deaf
Rox523 <www.feart39@gmail.com>
2024-09-22 14:55:11 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -3,6 +3,6 @@ first_meaning = 12
 second_name = "затрачено часов:"
 second_meaning = 1.5
 name = "Курс: Python,"
-print(1.5 / 12)
+time_for_one_task=(1.5 / 12)
 third_name = "среднее время выполнения 0,125 часа."
 print(name, first_name, first_meaning,',', second_name, second_meaning,',', third_name)
